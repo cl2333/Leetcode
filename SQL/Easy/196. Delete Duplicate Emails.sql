032a5f5690e7bead79133cde713a15053016b536
cl2333 <cl5293@nyu.edu>
2020-06-28 17:28:28 -0400

    UPDATE

---
+++ SQL/Easy/196. Delete Duplicate Emails.sql
@@ -0,0 +1,20 @@
+-- Write your MySQL query statement below
+DELETE FROM Person WHERE 
+id in (select id 
+from (
+select id, ROW_NUMBER() OVER (partition by email order by id asc) as ranking
+from person
+) t where t.ranking > 1
+      );
+
+-- simpler version
+With cte as
+(
+Select pp.id as id
+from person p
+join person pp
+on p.email = pp.email and p.id<pp.id)
+
+Delete from
+Person
+where ID IN (Select id from cte)s
---
+++ SQL/readme.md
@@ -0,0 +1,5 @@
+#Hard
+[1270. All People Report to the Given Manager * ]()
+
+#Medium
+[1270. All People Report to the Given Manager * (Recursive CTE)]()
